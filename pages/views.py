f2daaaf1683752cc81f3b5eb4dee6b65881b4a49
motazabdou <motazabdou30@gmail.com>
2022-02-01 21:18:51 +0000

    Added static folder

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, 'pages/index.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
