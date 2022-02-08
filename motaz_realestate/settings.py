72729115db53da5984139711e314f4800385f211
motazabdou <motazabdou30@gmail.com>
2022-02-08 17:31:16 +0000

    Fixed css background properties to correctly display images

--- motaz_realestate/settings.py
+++ motaz_realestate/settings.py
@@ -124,6 +124,8 @@ STATICFILES_DIRS = [
 ]
 
 # Default primary key field type
+# /workspace/realestate_project/motaz_realestate/static
+# static
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
