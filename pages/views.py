c02b03981c71810f695a2de21109d9543cc3d1ce
motazabdou <motazabdou30@gmail.com>
2022-02-22 18:17:51 +0000

    Added context to index view to dynamically render data from database

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
 
 def index(request):
-    return render(request, 'pages/index.html')
+    """ Render index page """
+    # [:number] limits entries to specified number
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings': listings
+    }
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
+    """ render about page """
     return render(request, 'pages/about.html')
+
