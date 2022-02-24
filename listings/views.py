47ac86276f410036fc570f867fe56c77f5e2141d
motazabdou <motazabdou30@gmail.com>
2022-02-24 19:37:13 +0000

    Renamed zipcode and city model fields in listings class to eircode and city respectively, and modified index and search templates accordingly

--- listings/views.py
+++ listings/views.py
@@ -30,6 +30,30 @@ def listing(request, listing_id):
 def search(request):
     """ View to render Search template """
     queryset_list = Listing.objects.order_by('-list_date')
+
+    # search by keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = (
+            queryset_list.filter(description__icontains=keywords) | 
+            queryset_list.filter(county__iexact=keywords) | 
+            queryset_list.filter(eircode__iexact=keywords) |
+            queryset_list.filter(suburb__iexact=keywords)
+            )
+    
+    # search by eircode
+    if 'eircode' in request.GET:
+        eircode = request.GET['eircode']
+        if eircode:
+            queryset_list = queryset_list.filter(eircode__iexact=eircode)
+    
+    # search by county
+    if 'county' in request.GET:
+        county = request.GET['county']
+        if county:
+            queryset_list = queryset_list.filter(county__iexact=county)
+
     paginator = Paginator(queryset_list, 6)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
