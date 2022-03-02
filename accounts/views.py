703a6f9c74d1a869905e95fbcc9e3173779aa1e0
motazabdou <motazabdou30@gmail.com>
2022-03-02 16:46:13 +0000

    Added login logic, and added conditionals in navbar partial depending on user authentication

--- accounts/views.py
+++ accounts/views.py
@@ -42,8 +42,19 @@ def register(request):
 def login(request):
     """ Render login template"""
     if request.method == "POST":
-        print('Login successful')
-        return redirect('dashboard')
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        # if user is found
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid username and/or password')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
