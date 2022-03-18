e70808da698f14157e6ac7910201bd36b27124ed
motazabdou <motazabdou30@gmail.com>
2022-03-18 20:20:20 +0000

    Added conditional statement in contact view to ensure logged in users do not submit enquiries about the same property more than once

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Contact
 from django.contrib import messages
+from .models import Contact
 
 def contact(request):
     """" Contact form """
@@ -13,6 +13,14 @@ def contact(request):
         message = request.POST['message']
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
+
+        # Check if the user has already made an inquiry
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, 'You have already enquired about this property')
+                return redirect('/listings/'+listing_id)
 
         contact = Contact(listing=listing, listing_id=listing_id, name=name, 
         email=email, phone=phone, message=message, user_id=user_id)
